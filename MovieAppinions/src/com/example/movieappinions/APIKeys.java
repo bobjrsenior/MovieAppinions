d2b180164e3e7d3fdca4102af176b3980bee4685
Bobjrsenior <bobjrsenior@gmail.com>
2015-04-26 12:02:20 -0400

    Added to the permissions and a class to put api keys in. Be sure not to commit the keys.

---
+++ MovieAppinions/src/com/example/movieappinions/APIKeys.java
@@ -0,0 +1,6 @@
+package com.example.movieappinions;
+
+public class APIKeys {
+	public static final String ROTTEN_TOMATOES_API_KEY = "INSERT_KEY";
+	public static final String PARSE_API_KEY = "INSERT_KEY";
+}
