6be46f8e27609a2131a26d2b0e2408ed1f8a5959
Bobjrsenior <bobjrsenior@gmail.com>
2015-04-27 17:46:52 -0400

    Split Parse keys into Client and Application key

--- MovieAppinions/src/com/example/movieappinions/APIKeys.java
+++ MovieAppinions/src/com/example/movieappinions/APIKeys.java
@@ -6,5 +6,6 @@ package com.example.movieappinions;
  */
 public class APIKeys {
 	public static final String ROTTEN_TOMATOES_API_KEY = "INSERT_KEY";
-	public static final String PARSE_API_KEY = "INSERT_KEY";
+	public static final String PARSE_APPLICATION_KEY = "INSERT_KEY";
+	public static final String PARSE_CLIENT_KEY = "INSERT_KEY";
 }
