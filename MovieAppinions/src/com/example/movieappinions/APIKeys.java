ab956389ebe73dc70c5479270c9fe92d277318dd
Bobjrsenior <bobjrsenior@gmail.com>
2015-04-27 12:55:50 -0400

    Added slight documentation to APIKeys.java

--- MovieAppinions/src/com/example/movieappinions/APIKeys.java
+++ MovieAppinions/src/com/example/movieappinions/APIKeys.java
@@ -1,5 +1,9 @@
 package com.example.movieappinions;
 
+/**
+ * Holds the API Keys to be used in the App
+ * DO NOT COMMIT CHANGES TO THIS FILE
+ */
 public class APIKeys {
 	public static final String ROTTEN_TOMATOES_API_KEY = "INSERT_KEY";
 	public static final String PARSE_API_KEY = "INSERT_KEY";
